1b79b267121f20c95c3d0bcfef4b93dd2431eca0
NikDm <respectedoff@yandex.ru>
2024-07-22 11:39:15 +0300

    исправление - размещение блоков кода

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let num = 266219;
 
+let cubeOfNum = additNum(num) ** 3;
 
 function additNum(num) {
+  let sum = 1;
 	num = String(num).split('');
-	let sum = 1;
 	
 	num.forEach((number) => {
 		sum *= Number(number);
 	})
+
 	return sum;
 }
 
 console.log(additNum(num));
-
-let cubeOfNum = additNum(num) ** 3;
 
 console.log(String(cubeOfNum).slice(0, 2));
