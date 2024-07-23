d63a7e8c03a60b86d1f90fa37430a45c2b646223
resAsus <respectedbro@yandex.ru>
2024-07-23 18:10:31 +0300

    урок 3 выполнен

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const title = 'My project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 700;
+'use strict'
+
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '1200');
 const rollback = 88;
-const fullPrice = 1000000;
-const adaptive = true;
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = +prompt('Сколько это будет стоить?');
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
 
-console.log(typeof(title));
-console.log(typeof(fullPrice));
-console.log(typeof(adaptive));
+const servicePrice2 = +prompt('Сколько это будет стоить?');
 
-console.log(screens.length);
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
-console.log(`Стоимость верстки экранов ${fullPrice} долларов`);
+const servicePercentPrice = Math.ceil(fullPrice - rollback);
+console.log(servicePercentPrice);
 
-console.log(screens.toLocaleLowerCase().split(', '));
+switch(true) {
+	case fullPrice > 30000:
+		alert('Даем скидку в 10%');
+		break;
+	case fullPrice > 15000 && fullPrice < 30000:
+		alert('Даем скидку в 5%');
+		break;
+	case fullPrice < 15000 && fullPrice > 0:
+		alert('Скидка не предусмотрена');
+		break;
+	default:
+		alert('Что то пошло не так');
+}
 
-console.log((fullPrice * (rollback/100)));
+// console.log(typeof(title));
+// console.log(typeof(fullPrice));
+// console.log(typeof(adaptive));
+// console.log(screens.length);
+// console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
+// console.log(`Стоимость верстки экранов ${fullPrice} долларов`);
+// console.log(screens.toLocaleLowerCase().split(', '));
+// console.log((fullPrice * (rollback/100)));
