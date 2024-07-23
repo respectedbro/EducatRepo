a64a3f876327aacb48279aa10f80697c2053d854
NikDm <respectedoff@yandex.ru>
2024-07-23 22:37:27 +0300

    lesson03 completed

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const adaptive = confirm('Нужен ли адаптив на сайте?');
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
 const servicePrice1 = +prompt('Сколько это будет стоить?');
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
-
 const servicePrice2 = +prompt('Сколько это будет стоить?');
 
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
@@ -17,13 +16,13 @@ const servicePercentPrice = Math.ceil(fullPrice - rollback);
 console.log(servicePercentPrice);
 
 switch(true) {
-	case fullPrice > 30000:
+	case fullPrice >= 30000:
 		alert('Даем скидку в 10%');
 		break;
-	case fullPrice > 15000 && fullPrice < 30000:
+	case fullPrice >= 15000 && fullPrice < 30000:
 		alert('Даем скидку в 5%');
 		break;
-	case fullPrice < 15000 && fullPrice > 0:
+	case fullPrice < 15000 && fullPrice >= 0:
 		alert('Скидка не предусмотрена');
 		break;
 	default:
