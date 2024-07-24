55c370bb7a1ba43d2fbd41e0d48c9a7d608e2feb
resAsus <respectedbro@yandex.ru>
2024-07-24 19:33:38 +0300

    исправление c.log дни => days.. , исп строгое равенство

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 'use strict'
 
-let lang = 'en';
-
-// if (lang === 'ru') {
-// 	console.log('Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс');
-// } else if (lang === 'en') {
-// 	console.log('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
-// }
-
-// switch(lang) {
-// 	case 'ru':
-// 		console.log('Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс');
-// 		break;
-// 	case 'en':
-// 		console.log('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun');
-// 		break;	
-// }
-
+const lang = 'en';
 const days = {
 	ru: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
 	en: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -24,6 +8,22 @@ const days = {
 console.log(days[lang]);
 
 
+if (lang === 'ru') {
+	console.log(days.ru);
+} else if (lang === 'en') {
+	console.log(days.en);
+}
+
+switch(lang) {
+	case 'ru':
+		console.log(days.ru);
+		break;
+	case 'en':
+		console.log(days.en);
+		break;	
+}
+
+
 const namePerson = 'Артём';
-const person = namePerson == 'Артём' ? 'директор' : namePerson == 'Александр' ? 'преподаватель' : 'студент';
+const person = namePerson === 'Артём' ? 'директор' : namePerson === 'Александр' ? 'преподаватель' : 'студент';
 console.log(person);
