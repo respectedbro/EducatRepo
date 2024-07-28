cf97eb11396a8a07d398f7003b3528e7c67dd666
resAsus <respectedbro@yandex.ru>
2024-07-28 16:31:11 +0300

    advLesson07 compl

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+const week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс',];
+let now = new Date().getDay();
+
+if (now === 0) {
+    now = 6;
+} else {
+    now -= 1;
+}
+
+week.forEach((elem, i) => {
+    if (i === 5 || i === 6) {
+        if (i === now) {
+            console.log(`Выходной ${elem} Сегодня`);
+        } else {
+            console.log(`Выходной ${elem}`);
+        }
+    } else if (i === now) {
+        console.log(`Сегодня ${elem}`);
+    } else {
+        console.log(elem);
+    }
+});
