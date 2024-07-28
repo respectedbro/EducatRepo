4056be2ed8acd324384ac4200b1c6c44aecdf960
resAsus <respectedbro@yandex.ru>
2024-07-28 14:22:08 +0300

    lesson07 compl

--- script.js
+++ script.js
@@ -1,120 +1,125 @@
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-let rollback = 88;
-let allServicePrices;
-let fullPrice;
-let servicePercentPrice;
-let service1;
-let service2;
+'use strict'
 
-const isNumber = function(num) {
-	return !isNaN(parseFloat(num)) && isFinite(num);
-}
+const appData = {
+	title: '',
+	screens: '',
+	screenPrice: 0,
+	adaptive: true,
+	rollback: 10,
+	allServicePrices: 0,
+	fullPrice: 0,
+	servicePercentPrice: 0,
+	service1: '',
+	service2: '',
 
-const isString = function(str) {
-	return typeof str === 'string' && str.trim() !== '';
-}
-
-const asking = function() {
-	do {
-		title = prompt('Как называется ваш проект?', 'Калькулятор вёрстки');
-		if (title === null) {
-			alert('Введите ответ');
-		}
-	} while (!isString(title));
-	
-	do {
-		screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-		if (screens === null) {
-			alert('Введите ответ');
-		}
-	} while (!isString(screens));
-
-	do {
-		screenPrice = prompt('Сколько будет стоить данная работа?');
-		if (screenPrice !== null) {
-			screenPrice = screenPrice.trim();
-		}
-	} while (screenPrice === null || !isNumber(screenPrice));
-
-	screenPrice = parseFloat(screenPrice);
-	adaptive = confirm('Нужен ли адаптив на сайте?');
-}
-
-const getAllServicePrices = function() {
-	let sum = 0;
-
-	for (let i = 0; i < 2; i++) {
-		let service;
+	isNumber: function (num) {
+		return !isNaN(parseFloat(num)) && isFinite(num)
+	},
+	isString: function (str) {
+		return typeof str === 'string' && str.trim() !== ''
+	},
+	asking: function () {
 		do {
-			service = prompt('Какой дополнительный тип услуги нужен?');
-			if (service === null) {
-				alert('Введите ответ');
+			appData.title = prompt(
+				'Как называется ваш проект?',
+				'Калькулятор вёрстки'
+			)
+			if (appData.title === null) {
+				alert('Введите ответ')
 			}
-		} while (service === null || !isString(service));
+		} while (!appData.isString(appData.title))
 
-		if (i === 0) {
-			service1 = service;
-		} else if (i === 1) {
-			service2 = service;
-		}
-
-		let servicePrice;
 		do {
-			servicePrice = prompt('Сколько это будет стоить?');
-			if (servicePrice !== null) {
-				servicePrice = servicePrice.trim();
+			appData.screens = prompt(
+				'Какие типы экранов нужно разработать?',
+				'Простые, Сложные, Интерактивные'
+			)
+			if (appData.screens === null) {
+				alert('Введите ответ')
 			}
-		} while (servicePrice === null || !isNumber(servicePrice));
+		} while (!appData.isString(appData.screens))
 
-		sum += parseFloat(servicePrice);
-	}
-	return sum;
+		do {
+			appData.screenPrice = prompt('Сколько будет стоить данная работа?')
+			if (appData.screenPrice !== null) {
+				appData.screenPrice = appData.screenPrice.trim()
+			}
+		} while (
+			appData.screenPrice === null ||
+			!appData.isNumber(appData.screenPrice)
+		)
+
+		appData.screenPrice = parseFloat(appData.screenPrice)
+		appData.adaptive = confirm('Нужен ли адаптив на сайте?')
+	},
+	getAllServicePrices: function () {
+		let sum = 0
+
+		for (let i = 0; i < 2; i++) {
+			let service
+			do {
+				service = prompt('Какой дополнительный тип услуги нужен?')
+				if (service === null) {
+					alert('Введите ответ')
+				}
+			} while (service === null || !appData.isString(service))
+
+			if (i === 0) {
+				appData.service1 = service
+			} else if (i === 1) {
+				appData.service2 = service
+			}
+
+			let servicePrice
+			do {
+				servicePrice = prompt('Сколько это будет стоить?')
+				if (servicePrice !== null) {
+					servicePrice = servicePrice.trim()
+				}
+			} while (servicePrice === null || !appData.isNumber(servicePrice))
+
+			sum += parseFloat(servicePrice)
+		}
+		return sum
+	},
+	getFullPrice: function () {
+		return +appData.screenPrice + appData.allServicePrices
+	},
+	getServicePercentPrices: function () {
+		return appData.fullPrice - appData.fullPrice * (appData.rollback / 100)
+	},
+	getTitle: function () {
+		return (
+			appData.title.trim()[0].toUpperCase() +
+			appData.title.trim().slice(1).toLowerCase()
+		)
+	},
+	getRollbackMessage: function (price) {
+		if (price >= 30000) {
+			return 'Даём скидку 10%'
+		} else if (price >= 15000 && price < 30000) {
+			return 'Даём скидку 5%'
+		} else if (price >= 0 && price < 15000) {
+			return 'Скидка не предусмотрена'
+		} else {
+			return 'Что-то пошло не так'
+		}
+	},
+	start: function () {
+		appData.asking()
+		appData.allServicePrices = appData.getAllServicePrices()
+		appData.fullPrice = appData.getFullPrice()
+		appData.servicePercentPrice = appData.getServicePercentPrices()
+		appData.title = appData.getTitle()
+		appData.logger()
+	},
+	logger: function () {
+		console.log(appData.fullPrice)
+		console.log(appData.servicePercentPrice)
+		for (let key in appData) {
+			console.log(key)
+		}
+	},
 }
 
-const showTypeOf = function(variable) {
-	console.log(variable, typeof(variable));
-}
-
-function getFullPrice() {
-	return screenPrice + allServicePrices;
-}
-
-function getServicePercentPrices() {
-	return fullPrice - rollback;
-}
-
-const getTitle = function() {
-	return title.trim()[0].toUpperCase() + title.trim().slice(1).toLowerCase();
-}
-
-const getRollbackMessage = function(price) {
-	if (price >= 30000) {
-		return 'Даём скидку 10%';
-	} else if (price >= 15000 && price < 30000) {
-		return 'Даём скидку 5%';
-	} else if (price >= 0 && price < 15000) {
-		return 'Скидка не предусмотрена';
-	} else {
-		return 'Что-то пошло не так';
-	}
-}
-
-asking();
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-servicePercentPrice = getServicePercentPrices();
-title = getTitle();
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-
-console.log('allServicePrices', allServicePrices);
-console.log(getRollbackMessage(fullPrice));
-console.log(screens.length);
-console.log(getServicePercentPrices());
-
-console.log(`Стоимость вёрстки экранов ${screenPrice} юани и Стоимость разработки сайта ${fullPrice} юани`);
+appData.start()
